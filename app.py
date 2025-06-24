690a563251b18fc93019af990a765e7bc27406ee
eshanmalyala <eshanmalyala@outlook.com>
2025-06-24 21:58:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
+# from flask import Flask
+# app = Flask(__name__)
+# @app.route("/")
+# def hello_world():
+#     return "Hello, World!"
+# if __name__ == "__main__":
+#     app.run(debug=True)  # debug=True enables debug mode for development
 from flask import Flask
+import os
+
 app = Flask(__name__)
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
-if __name__ == "__main__":
-    app.run(debug=True)  # debug=True enables debug mode for development
+
+@app.route('/')
+def hello():
+    return 'Hello from Cloud Run with Flask!'
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
